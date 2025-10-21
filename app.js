91e15a2191e100c1f1bc004a67b80dbbb2294dd1
juanlo01 <juanlorenzo87@hotmail.com>
2025-10-21 20:26:28 +0200

    cambios commit

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function mostrarMensaje() {
-  document.getElementById("mensaje").textContent = "¡Que tengas un gran dia!!";
+  document.getElementById("mensaje").textContent = "¡Que tengas un gran dia y llama a tu ser mas querido!!";
 }
